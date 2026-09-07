42a21aad1f8224d25281021d3a8c85fc3ff12bbf
Valeria <emailul_tau@example.com>
2026-09-07 18:21:27 +0300

    Am Finisat crearea primei pagini (nu are timer)

--- src/main/java/com/example/laboratorul1/HelloController.java
+++ src/main/java/com/example/laboratorul1/HelloController.java
@@ -1,8 +1,22 @@
 package com.example.laboratorul1;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 public class HelloController {
+    @FXML
+    private Label speakerName ;
+    @FXML
+    private Label dialogueText;
+    @FXML
+    private Button continueButton;
+    @FXML
+    private ImageView backgroundImage;
 
+    @FXML
+    private void onNextClick(){
+
+    }
 }
